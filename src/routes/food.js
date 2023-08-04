980f678086ef400d47b8c7d263007446ce0f769a
seongwong woo <dnqkr18@gmail.com>
2023-08-04 10:24:26 +0900

    rr

--- src/routes/food.js
+++ src/routes/food.js
@@ -11,8 +11,8 @@ router.delete('/:id', foodController.deleteFood);
 // food 수정
 router.put('/:id', foodController.updateFood);
 // food 옵션 추가
-router.post('/:foodId', foodController.createFoodOption);
+router.post('/:foodId/option', foodController.createFoodOption);
 // food 옵션 삭제
-router.delete('/:foodId', foodController.deleteFoodOption);
+router.delete('/:foodId/option', foodController.deleteFoodOption);
 
 module.exports = router;
